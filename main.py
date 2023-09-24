0f54bb74a962a81de79d72c438998672e89dcc99
Joy9001 <joymridha939@gmail.com>
2023-09-24 15:42:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+level = input("Do you want your password to be 'easy' or 'hard'?\n")
+
+password = ""
+for i in range(0,nr_letters):
+  password = password + random.choice(letters)
+
+for i in range(0,nr_symbols):
+  password = password + random.choice(symbols)
+
+for i in range(0,nr_numbers):
+  password = password + random.choice(numbers)
+
+if level == "hard":
+  password = "".join(random.sample(password, len(password)))
+
+print(f"Here is your password: {password}")
